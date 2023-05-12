45b574ccd42019363e40ef1df35db84bef55ccfd
easonng520 <121944894+easonng520@users.noreply.github.com>
2023-05-12 03:05:10 +0000

    tutorial to cat

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,22 @@ import { Routes, Route, Link } from "react-router-dom";
 //import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import AddTutorial from "./components/add-tutorial.component";
-import Tutorial from "./components/tutorial.component";
-import TutorialsList from "./components/tutorials-list.component";
+import AddCat from "./components/add-cat.component";
+import Cat from "./components/cat.component";
+import CatsList from "./components/cats-list.component";
 
 class App extends Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/tutorials"} className="navbar-brand">
+          <Link to={"/cats"} className="navbar-brand">
             bezKoder
           </Link>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link to={"/tutorials"} className="nav-link">
-                Tutorials
+              <Link to={"/cats"} className="nav-link">
+                Cats
               </Link>
             </li>
             <li className="nav-item">
@@ -31,10 +31,10 @@ class App extends Component {
 
         <div className="container mt-3">
           <Routes>
-            <Route path="/" element={<TutorialsList/>} />
-            <Route path="/tutorials" element={<TutorialsList/>} />
-            <Route path="/add" element={<AddTutorial/>} />
-            <Route path="/tutorials/:id" element={<Tutorial/>} />
+            <Route path="/" element={<CatsList/>} />
+            <Route path="/cats" element={<CatsList/>} />
+            <Route path="/add" element={<AddCat/>} />
+            <Route path="/cats/:id" element={<Cat/>} />
           </Routes>
         </div>
       </div>
